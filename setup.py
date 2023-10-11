5efd7c5525a3e45779d50c367173836277e22055
HaKa04 <kaspar.hui@bluewin.ch>
2023-10-11 10:09:54 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.6'
+DESCRIPTION = 'ECC Library'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="elliptic_curves_fq", 
+    version=VERSION,
+    author="Kaspar Hui",
+    author_email="<kaspar.hui@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    install_requires=[],
+    keywords=['python', 'ECC', 'finite Fields'],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
