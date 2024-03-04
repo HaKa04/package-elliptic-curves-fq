a77fa1f41aeff927c037f41b8acff23daacf4996
HaKa04 <kaspar.hui@bluewin.ch>
2024-03-04 20:38:37 +0100

    innit and setup version

--- app/elliptic_curves_fq/__init__.py
+++ app/elliptic_curves_fq/__init__.py
@@ -1,4 +1,3 @@
-from .src.ChinesischerRestsatz import * 
 from .src.elliptischeKurve import curve, curve_Fpn , Points
 from .src.endlicheKoerper_Fp import Fp
 from .src.endlicherKoerper_Fpn import Fpn
@@ -6,5 +5,5 @@ from .src.getrandomcurve import get_randomcurve
 from .src.find_irreducible_polynomial import get_irreductible_polynomial,is_irreductible
 from .src.Kurven import *
 from .src.Menezes_vanstone_Ascii import *
-from .src.fast_curve import start_point as fast_start_point, fast_Curve_Point, fast_F47_hoch_48
-from .src.division_polynomial_fast_curve import Division_Polynomial, Polynomial
+from .src.fast_curve import start_point
+from .src.fast_schooftest_multy_process import *
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.3'
+VERSION = '2.0.0'
 DESCRIPTION = 'ECC Library'
 
 with open("README.md", "r") as f:
